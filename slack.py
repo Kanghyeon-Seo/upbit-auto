adb4873da8439674e4e4e6325c11f3e856820bbe
Kanghyeon Seo <eden.seo82@gmail.com>
2022-04-24 21:29:16 +0900

    first commit

---
+++ slack.py
@@ -0,0 +1,12 @@
+import requests
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+
+myToken = "your token"
+
+post_message(myToken,"#upbit-auto","UPBIT")
---
+++ tickers.py
@@ -0,0 +1,5 @@
+import pyupbit
+
+# ticers 확인
+tickers = pyupbit.get_tickers()
+print(tickers)
